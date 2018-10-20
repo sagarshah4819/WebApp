55862dbdcce90a3ef84dc1cb4e32b20f6bd7efac
sagarshah4819 <ss3958@njit.edu>
2018-10-20 15:00:06 -0700

    Task: Created includes,pages and layouts directory.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
+    return view('pages.index');
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::post('/contact', function () {
     return view('welcome');
 });
